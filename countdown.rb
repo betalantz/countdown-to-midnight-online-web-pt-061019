d71a68a1279ddc06aa0e55d7350bfed3f04fb7e5
Lantz Warrick <betalantz@gmail.com>
2019-06-12 20:38:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,12 @@ def countdown(secs)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(secs)
+  counter = secs
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
